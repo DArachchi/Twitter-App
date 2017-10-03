51aa4f64ffe25f6cbdb07eaa209df2fd2eac89e6
Dino Ferrari <DArachchi@gmail.com>
2017-10-02 23:57:19 -0500

    Added angular-chart.js dependency and module

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -2,29 +2,23 @@
 var twitter = require('twitter');
 var keys = require('../../keys.js');
 
+var client = new twitter ({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
+
 module.exports = function(app) {
 
-    // GET route to pull tweets
-    app.get("/api/tweets/", function(req, res) {
-        var client = new twitter ({
-            consumer_key: keys.twitterKeys.consumer_key,
-            consumer_secret: keys.twitterKeys.consumer_secret,
-            access_token_key: keys.twitterKeys.access_token_key,
-            access_token_secret: keys.twitterKeys.access_token_secret
-        })
-        var params = {screen_name: "LewisHamilton"};
+    // GET route to pull tweets for specified user
+    app.get("/api/tweets/:id", function(req, res) {
+        var params = {screen_name: req.params.id};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
             if (error) {
                 console.log(error);
             } else {
-                output = "";
-                for (var i=0; i<20; i++) {
-                    if (tweets[i]) {
-                        var currentTweetData = tweets[i].created_at + " " + tweets[i].text + "\r\n";
-                        output = output + currentTweetData;
-                    }
-                }
-                res.send(output);
+                res.send(tweets);
             }
         });
     });
--- public/js/app.js
+++ public/js/app.js
@@ -4,5 +4,6 @@
 angular.module('twitterApp', [
   'ngRoute',
   'appRoutes',
-  'home'
+  'home',
+  'chart.js'
 ]);
