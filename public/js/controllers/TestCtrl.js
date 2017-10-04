15a39d6d665061f5614ff727aa1487834f1d10f3
Dino Ferrari <DArachchi@gmail.com>
2017-10-04 01:22:00 -0500

    Added libraries to integrate Material

--- public/js/controllers/TestCtrl.js
+++ public/js/controllers/TestCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('test', ['ngRoute'])
-  .controller('TestCtrl', ['$scope', '$routeParams', 'tweetFactory', function($scope, $routeParams, tweetFactory) {
+  .controller('TestCtrl', ['$scope', 'tweetFactory', function($scope, tweetFactory) {
     $scope.userSearch;
     $scope.submit = function() {
         tweetFactory.query({username: $scope.userSearch}, function(tweets) {
