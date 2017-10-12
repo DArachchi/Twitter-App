e654efa62daec02b73dc00fe8d729a9c25435407
Dino Ferrari <DArachchi@gmail.com>
2017-10-11 22:52:53 -0500

    Attempting to use process.env for Heroku deploy

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -10,10 +10,10 @@ var client = new twitter ({
 });
 */
 var client = new twitter ({
-    consumer_key: consumer_key,
-    consumer_secret: consumer_secret,
-    access_token_key: access_token_key,
-    access_token_secret: ccess_token_secret
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
 });
 
 module.exports = function(app) {
