a2bcfeefe0143c97471af16e5c2b3e7d476c4bb1
Dino Ferrari <DArachchi@gmail.com>
2017-10-11 22:50:15 -0500

    Changed twitter API config variables to pull from Heroku

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,12 +1,19 @@
 // Require Twitter NPM and keys for twitter
 var twitter = require('twitter');
-var keys = require('../../keys.js');
+/*var keys = require('../../keys.js');
 
 var client = new twitter ({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
+});
+*/
+var client = new twitter ({
+    consumer_key: consumer_key,
+    consumer_secret: consumer_secret,
+    access_token_key: access_token_key,
+    access_token_secret: ccess_token_secret
 });
 
 module.exports = function(app) {
