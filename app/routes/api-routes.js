620b46252b91d50c876d3112735f7060a3235359
Dino Ferrari <DArachchi@gmail.com>
2017-10-12 23:52:24 -0500

    Updated all views with Material card format

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,14 +1,6 @@
 // Require Twitter NPM and keys for twitter
 var twitter = require('twitter');
-/*var keys = require('../../keys.js');
 
-var client = new twitter ({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
-});
-*/
 var client = new twitter ({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
